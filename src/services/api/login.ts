aeef02af8fdcc234f12ec6af44d7466e3e44ce63
igor <igor@ark-in.info>
2022-10-27 18:16:53 +0700

    create request for login

--- src/services/api/login.ts
+++ src/services/api/login.ts
@@ -1,10 +1,20 @@
+import axios from "axios";
+import { loginError, loginFetch, loginSuccess } from "../../store/slices/login/login.slice";
+import { AppDispatch } from "../../store/store";
 import { IUserLogin } from "../../types/userTypes/IUserLogin";
+import { URL } from "../../utils/constants/url.constants";
 
 export const auth = (data: IUserLogin) => {
-    return () => {
+    return async (dispatch: AppDispatch) => {
         try {
+            dispatch(loginFetch())
+            const response = await axios.post(URL.login, data)
+            const result = response.data;
+            dispatch(loginSuccess(result.message))
             
+            console.log(result)
         } catch (error) {
+            dispatch(loginError(error))
             console.log(error)
         }
     }
---
+++ src/utils/constants/url.constants.ts
@@ -0,0 +1,5 @@
+const domain: string =  'http://localhost:5000';
+
+export const URL = {
+    login: domain + '/auth/login',
+}
